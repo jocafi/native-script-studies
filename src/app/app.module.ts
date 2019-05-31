3002f7627b8b61358c544678e22178cd20b3624f
João Araujo <joccaraujo@gmail.com>
2019-05-31 20:35:50 +0200

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ItemsComponent } from "./item/items.component";
+import { ItemDetailComponent } from "./item/item-detail.component";
+import { CurrentChallengeComponent } from '~/app/challenges/current-challenge/current-challenge.component';
+import { NativeScriptFormsModule } from 'nativescript-angular';
+import { StackComponent } from './layouts/stack/stack.component';
+import { FlexboxComponent } from './layouts/flexbox/flexbox.component';
+import { GridComponent } from './layouts/grid/grid.component';
+import { AbsoluteComponent } from './layouts/absolute/absolute.component';
+import { ChallengeEditComponent } from '~/app/challenges/challenge-edit/challenge-edit.component';
+
+// .JA. VERY IMPORTANT:
+// Uncomment and add to NgModule imports if you need to use two-way binding
+// import { NativeScriptFormsModule } from "nativescript-angular/forms";
+
+// Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
+// import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptModule,
+        NativeScriptFormsModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        ItemsComponent,
+        ItemDetailComponent,
+        CurrentChallengeComponent,
+        StackComponent,
+        FlexboxComponent,
+        GridComponent,
+        AbsoluteComponent,
+        ChallengeEditComponent
+    ],
+    providers: [],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+/*
+Pass your application module to the bootstrapModule function located in main.ts to start your app
+*/
+export class AppModule { }
---
+++ src/app/challenges/challenge-edit/challenge-edit.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'ns-challenge-edit',
+  templateUrl: './challenge-edit.component.html',
+  styleUrls: ['./challenge-edit.component.css'],
+  moduleId: module.id
+})
+export class ChallengeEditComponent {
+  @Output() input = new EventEmitter<string>();
+  challengeDescription = '';
+  activeChallenge = '';
+
+  onSetChallenge() {
+    console.log('onSetChallenge: ', this.challengeDescription);
+    this.activeChallenge = this.challengeDescription;
+    // this.input.emit(this.challengeDescription);
+  }
+}
